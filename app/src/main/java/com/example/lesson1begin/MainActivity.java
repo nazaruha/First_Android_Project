e54886756362716f5e7447c5194d982ab48a2706
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-10-17 14:21:14 +0200

    start making calculator

--- app/src/main/java/com/example/lesson1begin/MainActivity.java
+++ app/src/main/java/com/example/lesson1begin/MainActivity.java
@@ -6,19 +6,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText txtInfo; // init property for some purpose
+    //private EditText txtInfo; // init property for some purpose
+    private TextView txtOutputOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtInfo = findViewById(R.id.txtInfo); // get object by id
+        //txtInfo = findViewById(R.id.txtInfo); // get object by id
+        txtOutputOperation = findViewById(R.id.txtOutputOperation);
+        txtOutputOperation.setText("Hello, World!");
     }
 
-    public void OnClickHandler(View view) {
-        String text = txtInfo.getText().toString();
-        Log.d("my-tag", "-----Нажали кнопку-----" + text);
+//    public void OnClickHandler(View view) {
+//        String text = txtInfo.getText().toString();
+//        Log.d("my-tag", "-----Нажали кнопку-----" + text);
+//    }
+
+    public void OnClearAllClickHandler(View view) {
+        txtOutputOperation.setText("");
+        Log.d("my-tag", "-----btn_ClearAll click-----");
     }
 }
